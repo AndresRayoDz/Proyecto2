e4266de7e1476ab82aba45a0adb1d127cd7829d6
Valerin <kbran1409@gmail.com>
2021-10-22 17:52:00 -0600

    Se crea clase arbol, en la cual estara la escructura de este

---
+++ ArbolB.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Valerine
+ */
+public class ArbolB {
+   
+    private class Nodo {
+    
+        // Atributos del arbol
+        public Nodo padre;
+        public Nodo derecha;
+        public Nodo izquierda;
+        
+        
+    }
+}
