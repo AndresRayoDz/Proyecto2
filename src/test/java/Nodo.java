d8bcdfbe9e9b23aab2ba8150812e40563aaa3d23
VKCB <kbran1409@gmail.com>
2021-11-04 03:56:00 +0100

    Se crea la clase nodo

---
+++ src/test/java/Nodo.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Valerine
+ */
+public class Nodo {
+    private Nodo padre; 
+    private Nodo nodoizq;
+    private Nodo nodader;
+}
